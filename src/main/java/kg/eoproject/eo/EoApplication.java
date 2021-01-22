556d952586ad0eb1e2f7f15507e46c8372206d69
orozbek01 <orozbek.01@gmail.com>
2021-01-22 13:31:15 +0300

    Initial commit

---
+++ src/main/java/kg/eoproject/eo/EoApplication.java
@@ -0,0 +1,13 @@
+package kg.eoproject.eo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/eoproject/eo/EoApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.eoproject.eo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
